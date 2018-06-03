0fa69818a4113f8d608d124238de413567e8706b
Unknown <bc95.neus@gmail.com>
2018-06-03 18:22:47 +0200

    PlotMAPA (IP-Country)

--- projecte_datadriven.R
+++ projecte_datadriven.R
@@ -1,73 +1,70 @@
-#instala paquets necessaris
+#Instalación de paquetes necesarios
 install.packages("iptools")
 install.packages("stringr")
 
-#carrega les llibreries necessaries en el workspace
+#Cargar las librerias necesarias
 library(iptools)
 library(stringr)
 
-#agafa el working directory i el fica a "mainDir" i el subdirectori DownloadedData a "subDir"
+#Asignación variables para working directory y DownloaodedData.
 mainDir <- getwd()
 subDir <- "DownloadedData"
 
-#crea la carpeta downloadeddata al main directory, seteja el working directory a aquest i ho posa a la variable "downloadsdirectory"
-dir.create(file.path(mainDir, subDir))
-setwd(paste(mainDir,subDir, sep = '/'))
-downloadsDirectory <- getwd()
+dir.create(file.path(mainDir, subDir)) #creación carpeta "DownloadedData" al main directory
+setwd(paste(mainDir,subDir, sep = '/')) #set del working directori de la carpeta
+downloadsDirectory <- getwd() #guardar en la variable downloadsDirectory
 
-#fica a "font" la URL base i la descarrega a "links"
-font <- "http://dns-bh.sagadc.org/"
-links <- readLines(font)
 
-#fica a "patro" una regex que detecta enllasos i els extreu a "links2"
-patro <- 'href=\\"([[:alnum:]\\.]*)'
-links2 <- str_extract(links,patro)
+fuente <- "http://dns-bh.sagadc.org/" #asignación URL a la variable "fuente"
+links1 <- readLines(fuente) #lee cada linea de la "fuente" y la asigna a "links1".
 
-#treiem "href=\" dels links i els fiquem a links3
-links3 <- str_remove(links2,'href=\"')
+patron <- 'href=\\"([[:alnum:]\\.]*)' #regex para detectar enlaces
+links2 <- str_extract(links1,patron) #extraer el resultado en "links2".
+
+links3 <- str_remove(links2,'href=\"') #eliminar "href=\" de links2 y guardar en links3
 
 #iterem pels links de links3 i afegim la URL base per crear URL's completes (pero mal creades)
-#linksss <- sapply(links3, function(link){
+#links <- sapply(links3, function(link){
 #  paste(font, link, sep="")
 #})
 
 #iterem pels links de links3 i afegim la URL base per crear URL's completes pero amb 2 barres (//)
-#linksss <- sapply(links3, function(link){
+#links <- sapply(links3, function(link){
 #  paste(font, link, sep="/")
 #})
 
-#iterem pels links de links3 i afegim la URL base per crear URL's completes correctes
+#Iteración para links3: añadir la URL base para que devuelva las URL's completas en formato correcto
 full_links <- sapply(links3, function(link){
-  paste(font, link, sep="")
+  paste(fuente, link, sep="")
 })
 
-#busquem el patro "txt" al vector full_links i creem un vector de "True" i "False"
+#Búsqueda patrón "txt" en el vector full_links, devolviendo un vector de "True/False"
 iters <- grepl(pattern = "txt", x = full_links)
 
-#fiquem a "full_links_filtered" els links que tenien un "txt"
+#Guardar los links con "txt" en la variable "full_links_filtered"
 full_links_filtered <- full_links[iters]
 
-#Treiem qualsevol link que tingui les paraules .zip
+#Eliminar links que contengan la extensión "zip"
 any(grepl("zip", full_links_filtered))
 
-#iterem per "full_links_filtered" i descarreguem tots els arxius al working directory
+#Iteración para descargar todos los archivos de "full_links_filtered" y guardarlos al working directory
 sapply(full_links_filtered, function(link) {
   download.file(url = link, destfile = paste(downloadsDirectory,basename(link),sep = "/") )
 })
 
-#esborrem arxius innecessaris
+#Borrar archivos innecesários
 file.remove("test.txt")
 file.remove("freewebhosts.txt")
 file.remove("pushdo.txt")
 file.remove("domains.txt")
 
-#listem els arxius descarregats a "downloaded_files"
+#Listar archivos descargados a "downlodaded_files"
 downloaded_files <- list.files(downloadsDirectory)
 
-#inicialitzo "file2read" amb el primer arxiu de la llista
+#Guardar el primer archivo de downloaded_files en "file2read"
 file2read <- downloaded_files[[1]]
 
-#aqui hauriem de llegir tots els arxius amb un sapply o lapply i ficarlos tots al dataframe
+#Leer todos los archivos(sapply o lapply) y añadirlos al dataframe
 ##################################################################################
 #################AREA DE PROVES###################################################
 sapply(downloaded_files, function(file2read){
@@ -91,15 +88,51 @@ sapply(downloaded_files, function(file2read){
 df2 <-read.csv(file = paste(downloadsDirectory,file2read, sep = "/"), sep = "", header = FALSE, fill = TRUE, col.names = c("domain", "date", "type", "origin", "1", "2", "3", "4"), stringsAsFactors = F)
 
 ?read.csv
-df <- read.table(paste(getwd(),"20150825.txt", sep = "/"), col.names = c("domain", "date"), stringsAsFactors = F)
+dftest<- read.table(paste(getwd(),"20150825.txt", sep = "/"), col.names = c("domain", "date"), stringsAsFactors = F)
 ##################################################################################################
 #####################################################################################################
 
-#defineix la columna de la data com a tipus data
-df$date <- as.Date(as.character(df$date), "%Y%m%d") 
+#Definir la columna de la fecha como tipo fecha para obtener el formato YYYY/MM/D.
+dftest$date <- as.Date(as.character(dftest$date), "%Y%m%d") 
 
-#busca la IP del hostname cridant a la funcio hostname_to_ip amb la columna domain del dataframe
-df$ip <- hostname_to_ip(df$domain)
+#Búsqueda de la IP del hostname, a través de la columna domain del Dataframe, mediante la función hostname_to_ip
+dftest$ip <- hostname_to_ip(dftest$domain)
 
-#deixa nomes la primera IP de cada domini si hi ha mes de una
-df$ip2 <- sapply(df$ip, '[[', 1)
+#Para los casos que un dominio tenga más de una IP, guardar solo la primera.
+dftest$ip2 <- sapply(dftest$ip, '[[', 1)
+
+
+
+#Crea un Data frame con todas las caracteristicas de las IP (rgeolocate)
+dfIP1<- ip_api(dftest$ip2, as_data_frame = TRUE, delay = FALSE)
+#Eliminamos columnas que no nos interesan y guardamos la nueva data.frame en dfIP
+cols.dont.want <- c("as_code", "country_code", "isp", "latitude", "longitude", "organisation", "region_code", "region_name", "timezone", "zip_code")
+dfIP <- dfIP1[, ! names(dfIP1) %in% cols.dont.want, drop = F]
+#Renombramos la columna de country_name
+library(data.table)
+setnames(dfIP, old=c("city_name","country_name"), new=c("Cities", "Countries"))
+
+#Unimos data frame: dfIP con dftest en dftest1--> para juntar TODOS LOS DATOS*****
+dftest1 = cbind(dftest, IP)
+View (dftest1)
+
+# en el mapa se usará finalmente con el dframe de TODOS LOS DATOS*****
+#Mostrar en mapa las IP: rworldmap vignette
+library(rworldmap)
+par(mai=c(0,0,0.2,0), xaxs="i", yaxs="i")
+#Adjuntar los datos al country map
+sPDF<- joinCountryData2Map(dfIP,
+                           joinCode = "NAME",
+                           nameJoinColumn ="Countries" )
+#mapping
+mapCountryData(sPDF, 
+               nameColumnToPlot = "Countries",
+               mapTitle = 'Country risk',
+               oceanCol = 'lightblue',
+               missingCountryCol = 'white')
+
+
+
+
+
+
