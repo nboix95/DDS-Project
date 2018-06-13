1a5c4828b1820ea87aa89572306a9b19b7c9db47
Unknown <bc95.neus@gmail.com>
2018-06-13 20:51:26 +0200

    Country map done

--- projecte_datadriven.R
+++ projecte_datadriven.R
@@ -1,4 +1,4 @@
-#Instalaciï¿½n de paquetes necesarios
+#Instalacion paquetes necesarios.
 install.packages("iptools")
 install.packages("stringr")
 
@@ -6,16 +6,16 @@ install.packages("stringr")
 library(iptools)
 library(stringr)
 
-#Asignaciï¿½n variables para working directory y DownloaodedData.
+#Asignacion variables para working directory y DownloaodedData.
 mainDir <- getwd()
 subDir <- "DownloadedData"
 
-dir.create(file.path(mainDir, subDir)) #creaciï¿½n carpeta "DownloadedData" al main directory
+dir.create(file.path(mainDir, subDir)) #creacion carpeta "DownloadedData" al main directory
 setwd(paste(mainDir,subDir, sep = '/')) #set del working directori de la carpeta
 downloadsDirectory <- getwd() #guardar en la variable downloadsDirectory
 
 
-fuente <- "http://dns-bh.sagadc.org/" #asignaciï¿½n URL a la variable "fuente"
+fuente <- "http://dns-bh.sagadc.org/" #asignacion URL a la variable "fuente"
 links1 <- readLines(fuente) #lee cada linea de la "fuente" y la asigna a "links1".
 
 patron <- 'href=\\"([[:alnum:]\\.]*)' #regex para detectar enlaces
@@ -33,26 +33,26 @@ links3 <- str_remove(links2,'href=\"') #eliminar "href=\" de links2 y guardar en
 #  paste(font, link, sep="/")
 #})
 
-#Iteraciï¿½n para links3: aï¿½adir la URL base para que devuelva las URL's completas en formato correcto
+#Iteracion para links3: añadir la URL base para que devuelva las URL's completas en formato correcto
 full_links <- sapply(links3, function(link){
   paste(fuente, link, sep="")
 })
 
-#Bï¿½squeda patrï¿½n "txt" en el vector full_links, devolviendo un vector de "True/False"
+#Busqueda patron "txt" en el vector full_links, devolviendo un vector de "True/False"
 iters <- grepl(pattern = "txt", x = full_links)
 
 #Guardar los links con "txt" en la variable "full_links_filtered"
 full_links_filtered <- full_links[iters]
 
-#Eliminar links que contengan la extensiï¿½n "zip"
+#Eliminar links que contengan la extension "zip"
 any(grepl("zip", full_links_filtered))
 
-#Iteraciï¿½n para descargar todos los archivos de "full_links_filtered" y guardarlos al working directory
+#Iteracion para descargar todos los archivos de "full_links_filtered" y guardarlos al working directory
 sapply(full_links_filtered, function(link) {
   download.file(url = link, destfile = paste(downloadsDirectory,basename(link),sep = "/") )
 })
 
-#Borrar archivos innecesï¿½rios
+#Borrar archivos innecesarios
 file.remove("test.txt")
 file.remove("freewebhosts.txt")
 file.remove("pushdo.txt")
@@ -64,59 +64,67 @@ downloaded_files <- list.files(downloadsDirectory)
 #Guardar el primer archivo de downloaded_files en "file2read"
 file2read <- downloaded_files[[1]]
 
-#combina tots els arxius en un mateix dataframe en 7 columnes
+#Combina todos los archivos en un mismo dataframe (7 columnas)
 mainDataFrame <- do.call("rbind", lapply(downloaded_files, function(file2read){
   read.csv(file = paste(downloadsDirectory, file2read, sep = "/"), sep = "", header = F, fill = T, stringsAsFactors = F, col.names = c("domain", "type", "origin", "date", "1", "2", "3"))
 }))
 
-#Agafa la ultima columna NO N/A i la posa a Data
+#Coger la ultima columna (sin N/A) y la añade a Data Frame
 mainDataFrame$date <- apply(mainDataFrame, 1, function(x) tail(na.omit(x), 1))
 
-#defineix la columna de la data com a tipus data
+#Definir la columna del data frame como tipo data
 mainDataFrame$date <- as.Date(as.character(mainDataFrame$date), "%Y%m%d") 
 
-#Borra les columnes sobrants
+#Borrar las columnas restantes
 mainDataFrame[5:7] <- list(NULL)
 
-#Borra linies mal formatejades buscant un "origin" o una "data" buit
+#Borrar lineas mal formateadas buscando un "origin" o "dato" vacio
 mainDataFrame <- mainDataFrame[!(mainDataFrame$origin==""),]
 mainDataFrame <- mainDataFrame[!(is.na(mainDataFrame$date)),]
 
-#busca la IP del hostname cridant a la funcio hostname_to_ip amb la columna domain del dataframe
+#Buscar la IP del hostname (columna domain del dataframe) usando la función hostname_to_ip
 mainDataFrame$ip <- hostname_to_ip(mainDataFrame$domain)
 
-#deixa nomes la primera IP de cada domini si hi ha mes de una
+#En caso de que haya más de una IP, dejar la primera
 mainDataFrame$ip <- sapply(mainDataFrame$ip, '[[', 1)
 
-#guarda el dataframe a un arxiu local
+#Guardar el dataframe, en un archivo local
 save(mainDataFrame,file = "mainDataframe.Rda")
 
 testframe <- mainDataFrame[dim(mainDataFrame)[1]:1,]
 save(testframe, file = "testframe.Rda")
 
 
+#Sacar las filas que tengan IP not resolved y guardarlo en otro dataframe
+mainDataFrame1<- subset(mainDataFrame, ip != "Not resolved")
 
 #Crea un Data frame con todas las caracteristicas de las IP (rgeolocate)
-dfIP1<- ip_api(dftest$ip2, as_data_frame = TRUE, delay = FALSE)
-#Eliminamos columnas que no nos interesan y guardamos la nueva data.frame en dfIP
-cols.dont.want <- c("as_code", "country_code", "isp", "latitude", "longitude", "organisation", "region_code", "region_name", "timezone", "zip_code")
-dfIP <- dfIP1[, ! names(dfIP1) %in% cols.dont.want, drop = F]
+install.packages("rgeolocate")
+library(rgeolocate)
+
+file <- system.file("extdata","GeoLite2-Country.mmdb", package = "rgeolocate")
+results <- maxmind(mainDataFrame1$ip, file, "country_name")
+
 #Renombramos la columna de country_name
+install.packages(data.table)
 library(data.table)
-setnames(dfIP, old=c("city_name","country_name"), new=c("Cities", "Countries"))
+setnames(results, old="country_name", new="Countries")
 
 #Unimos data frame: dfIP con dftest en dftest1--> para juntar TODOS LOS DATOS
-dftest1 = cbind(dftest, IP)
-View (dftest1)
+mainDataFrameIP = cbind(mainDataFrame1, results)
+View (mainDataFrameIP)
 
-#data frame: dfIP para el mapa
+#Eliminar filas con valor NA
+mainDFIP<-na.omit(mainDataFrameIP)
+
+#data frame: mainDFIP para el mapa
 #Mostrar en mapa las IP: rworldmap vignette
 library(rworldmap)
 par(mai=c(0,0,0.2,0), xaxs="i", yaxs="i")
 #Adjuntar los datos al country map
-sPDF<- joinCountryData2Map(dfIP,
-                           joinCode = "NAME",
-                           nameJoinColumn ="Countries" )
+sPDF<- joinCountryData2Map(mainDFIP,
+                          joinCode = "NAME",
+                          nameJoinColumn ="Countries" )
 #mapping
 mapCountryData(sPDF, 
                nameColumnToPlot = "Countries",
