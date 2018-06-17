8678af0f2a39ebe9903ba1f2e5e974bf9979cd70
Unknown <bc95.neus@gmail.com>
2018-06-17 15:36:32 +0200

    Gráfico válido ataques/dia

--- projecte_datadriven.R
+++ projecte_datadriven.R
@@ -1,10 +1,18 @@
 #Instalacion paquetes necesarios.
 install.packages("iptools")
 install.packages("stringr")
+install.packages("rgeolocate")
+install.packages("data.table")
+install.packages("plyr")
+
 
 #Cargar las librerias necesarias
 library(iptools)
 library(stringr)
+library(rgeolocate)
+library(data.table)
+library(plyr)
+library(datasets)
 
 #Asignacion variables para working directory y DownloaodedData.
 mainDir <- getwd()
@@ -22,16 +30,6 @@ patron <- 'href=\\"([[:alnum:]\\.]*)' #regex para detectar enlaces
 links2 <- str_extract(links1,patron) #extraer el resultado en "links2".
 
 links3 <- str_remove(links2,'href=\"') #eliminar "href=\" de links2 y guardar en links3
-
-#iterem pels links de links3 i afegim la URL base per crear URL's completes (pero mal creades)
-#links <- sapply(links3, function(link){
-#  paste(font, link, sep="")
-#})
-
-#iterem pels links de links3 i afegim la URL base per crear URL's completes pero amb 2 barres (//)
-#links <- sapply(links3, function(link){
-#  paste(font, link, sep="/")
-#})
 
 #Iteracion para links3: añadir la URL base para que devuelva las URL's completas en formato correcto
 full_links <- sapply(links3, function(link){
@@ -84,7 +82,6 @@ mainDataFrame <- mainDataFrame[!(is.na(mainDataFrame$date)),]
 
 #Buscar la IP del hostname (columna domain del dataframe) usando la función hostname_to_ip
 mainDataFrame$ip <- hostname_to_ip(mainDataFrame$domain)
-totalIP <- as.data.frame(hostname_to_ip(mainDataFrame$domain))
 
 #En caso de que haya más de una IP, dejar la primera
 mainDataFrame$ip <- sapply(mainDataFrame$ip, '[[', 1)
@@ -99,26 +96,20 @@ save(testframe, file = "testframe.Rda")
 #Sacar las filas que tengan IP not resolved y guardarlo en otro dataframe
 mainDataFrame1<- subset(mainDataFrame, ip != "Not resolved")
 
-#Crea un Data frame con todas las caracteristicas de las IP (rgeolocate)
-install.packages("rgeolocate")
-library(rgeolocate)
-
+#Crea un Data frame con todos los países de las IP's (rgeolocate)
 file <- system.file("extdata","GeoLite2-Country.mmdb", package = "rgeolocate")
 results <- maxmind(mainDataFrame1$ip, file, "country_name")
 
 #Renombramos la columna de country_name
-install.packages(data.table)
-library(data.table)
 setnames(results, old="country_name", new="Countries")
 
-#Unimos data frame: dfIP con dftest en dftest1--> para juntar TODOS LOS DATOS
+#Unimos data frame "results" con mainDataFrame1, siendo el resultado: mainDataFrameIP
 mainDataFrameIP = cbind(mainDataFrame1, results)
 View (mainDataFrameIP)
 
 #Eliminar filas con valor NA
-mainDFIP<-na.omit(mainDataFrameIP)
+mainDFIP<-na.omit(mainDataFrameIP) #data frame mainDFIP: para el mapa
 
-#data frame: mainDFIP para el mapa
 #Mostrar en mapa las IP: rworldmap vignette
 library(rworldmap)
 par(mai=c(0,0,0.2,0), xaxs="i", yaxs="i")
@@ -132,34 +123,25 @@ mapCountryData(sPDF,
                oceanCol = 'lightblue',
                missingCountryCol = 'white')
 
-#Proveedor IP (whois): domaintools-> se tiene que pagar 100 euros...
-#install.packages("devtools")
-#devtools::install_github("hrbrmstr/domaintools")
-#library(domaintools) 
-
-#Contar num de veces que se repite cada país
-install.packages("plyr")
-library(plyr)
+#Contar número de veces que se repite cada país
 resultsIP<-na.omit(results)
 x = count(resultsIP, 'Countries')
 
-#Contar cuantas webs vulneradas hay por mes y año
+#Contar cuantas webs vulneradas hay por mes de cada año
 fechas=as.Date(mainDataFrame$date)
 Año <-format(fechas,"%Y%m")
 xtiempo <- as.data.frame(Año)
 dfyearmonth = count(xtiempo, "Año")
 
-
-
-#Contar frecuencia de tiempo
+#Contar frecuencia de tiempo (por día)
 tiempo = count(mainDataFrame, 'date')
-#Histograma tiempo
-#library(datasets)
-#hist(tiempo$freq)
 
 #Histograma tiempo con estimador de núcleo de la densidad
-library(datasets)
 hist(dfyearmonth$freq,freq=FALSE,col="lightcyan", ylim=c(0,0.0003),main="Histograma de ataques",xlab="")
 lines(density(dfyearmonth$freq),col="red",lwd=2)
 
-#hist(tiempo$freq)
+#Otro histograma: 
+hist(dfañomes$freq, main="Histograma de ataques respecto al tiempo", col=1:10, breaks=41)
+
+#tabla
+barplot(prop.table(table(mainDataFrame$date)), main = "Histograma de ataques diarios 2013-2018", ylim = c(0,0.012))
